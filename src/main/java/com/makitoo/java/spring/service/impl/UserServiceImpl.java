0835d27ceead13f4bf7ed0da0ec6702c5791ddc2
Maxime Clement <m.clement@makitoo.com>
2017-03-20 11:26:25 +0100

    Before instrumentation

--- src/main/java/com/makitoo/java/spring/service/impl/UserServiceImpl.java
+++ src/main/java/com/makitoo/java/spring/service/impl/UserServiceImpl.java
@@ -34,7 +34,10 @@ public class UserServiceImpl implements UserService {
     public User save(User user) {
         String id = UUID.randomUUID().toString();
         user.setId(id);
+
+        // FIXME: lastName can be null
         user.setLastname(user.getLastname().toUpperCase());
+
         users.put(id, user);
         return user;
     }
